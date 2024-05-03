1d86d535135e461777f16403409ad5a2c9a395f3
thugginnn <paul.iyiade4@gmail.com>
2024-05-03 01:07:32 +0000

    0

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""A module that contains a function that adds two integers."""
+
+
+def add(a: float, b: float) -> float:
+    """ Puts two integers together."""
+    return a + b
