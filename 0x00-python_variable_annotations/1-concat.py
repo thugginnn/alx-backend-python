ae54d3dc54ca3d0532c1181ed70a0fc1d456e712
thugginnn <paul.iyiade4@gmail.com>
2024-05-03 01:10:15 +0000

    1c

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Contains a single function that links two strings."""
+
+
+def concat(str1: str, str2: str) -> str:
+    """Connects two strings."""
+    return str1 + str2
